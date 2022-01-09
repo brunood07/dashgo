977922c6442800afd5f838ab2a257ae6161cd1e3
Bruno Domingues <brunood07@gmail.com>
2022-01-09 14:12:31 -0300

    responsividade aplicada no header, sidebar e listagem de usuários

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,13 +1,19 @@
 import { Flex, Box, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Bruno Domingues</Text>
-        <Text color="gray.300" fontSize="small">brunood07@gmail.com</Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Bruno Domingues</Text>
+          <Text color="gray.300" fontSize="small">brunood07@gmail.com</Text>
+        </Box>
 
+      )}
       <Avatar size="md" name="Bruno Domingues" src="https://github.com/brunood07.png"/>
     </Flex>
   );
